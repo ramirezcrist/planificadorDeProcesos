cee4f51298d7ccc4abb04cf3fd9bb93b4f995a8f
ramirezcrist200924468 <ramirezcrist@gmail.com>
2017-12-12 16:25:40 -0600

    agregand cliente y servidor con socket

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/node_modules'));
+app.set("view engine", "jade");
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname+'/views/indexsocket.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+        client.emit('messages', 'Hello from server');
+    });
+
+    client.on('messages', function(data) {
+      funcion();
+           client.emit('broad', data);
+           client.broadcast.emit('broad',data);
+    });
+    });
+
+    /*
+funcion(){
+
+var resultado =  exec("egrep ... menfino");
+resultado = 1111 ;
+
+}
+
+
+    */
+
+
+server.listen(3000);
