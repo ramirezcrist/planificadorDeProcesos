584a866037936a4e9e5a0e88743c473790eeced7
ramirezcrist200924468 <ramirezcrist@gmail.com>
2017-12-17 23:29:52 -0600

    Entrega 1 practica 1

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var fs = require('fs');//Para agregar FileSystem
-var exec = require('child_process').exec,child, child1;;
+var exec = require('child_process').exec,child, child1, childPrincipal;;
+
+var contadorActivos = 0;//R
+var contadorSleep = 0;//S
+var contadorsTopped = 0;//T
+var contadorZombies = 0;//Z
 
 app.use(express.static(__dirname + '/node_modules'));
 app.set("view engine", "jade");
@@ -27,6 +32,7 @@ io.on('connection', function(client) {
   setInterval(function(){
     updateProcessInfo();
     var lineas = funcionProcesos();
+    var salida = "Total procesos = " + lineas + " Total sleeping: "+ contadorSleep;
     client.emit('broad', lineas);
   }, 2000);//fin interaval.. 2 milisegundo
 }); //fin io
@@ -43,10 +49,49 @@ function updateProcessInfo(){
   });
 }
 
-function funcionProcesos(){
+function funcionProcesos(){//devuelve numero de lineas: osea numero de procesos
   var contents = fs.readFileSync('/home/ubuntu/proyecto/lista.txt');
   var lines = contents.toString().split('\n').length - 1;
+  var cont = 0;
+
+  while (cont<lines) {
+    var direccion = "cp /proc/" + lines[cont] + "/stat "+ "/home/ubuntu/proyecto/descripcionProceso.txt");
+    childPrincipal = exec(direccion);
+    numeroProcesosActivos();
+
+    cont++;
+  }
   console.log(lines);
+  return lines;
 }
 
-server.listen(3000);
+function numeroProcesosActivos(){//muestra el numero de procesos activos
+  var contents = fs.readFileSync('/home/ubuntu/proyecto/descripcionProceso.txt');
+
+  var lines = contents.toString().split('\n');
+    if(lines[2].indexOf("R") != -1){//esta en runnable
+        contadorActivos= contadorActivos +1;
+    }
+    if(lines[2].indexOf("S") != -1){//esta en sleep
+        contadorSleep= contadorSleep + 1;
+    }
+    if(lines[2].indexOf("T") != -1){//esta en sTopped
+        contadorsTopped= contadorsTopped + 1;
+    }
+    if(lines[2].indexOf("Z") != -1){//esta en zombie
+        contadorsTopped= contadorsTopped + 1;
+    }
+  //ejectuar ps "num proceso"
+}
+
+server.listen(4000);
+
+/*
+Status del proceso, estos pueden ser los siguientes
+R runnable, en ejecución, corriendo o ejecutándose
+S sleeping, proceso en ejecución pero sin actividad por el momento, o esperando por algún evento para continuar
+T sTopped, proceso detenido totalmente, pero puede ser reiniciado
+Z zombie, difunto, proceso que por alguna razón no terminó de manera correcta, no debe haber procesos zombies
+D uninterruptible sleep, son procesos generalmente asociados a acciones de IO del sistema
+X dead, muerto, proceso terminado pero que sigue apareciendo, igual que los Z no deberían verse nunca
+*/
